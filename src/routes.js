b4007bf24067040aa4f99c9730778bcd4235026c
Alexander Augusto <alexaasf_10@hotmail.com>
2020-07-31 13:45:39 -0300

    finalize the first version of backend

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const assistant = require('./utils/assistent')
+const axios = require('axios')
+const btoa = require('btoa')
+const request = require('request')
+const { getDate, getEveryScore } = require('./utils/util')
 
 const routes = express.Router()
 
@@ -39,6 +43,93 @@ routes.post('/assistent/message', (req, res) => {
       res.json({ message })
     })
     .catch(err => handleError(res, err));
+})
+
+routes.get('/get-iam-token', async (req, res) => {
+  const IBM_Cloud_IAM_uid = "bx";
+  const IBM_Cloud_IAM_pwd = "bx";
+
+  const options = {
+    url: "https://iam.bluemix.net/oidc/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Basic " + btoa(IBM_Cloud_IAM_uid + ":" + IBM_Cloud_IAM_pwd)
+    },
+    body: "apikey=" + process.env.WATSON_MACHINE_LEARNING_API_KEY + "&grant_type=urn:ibm:params:oauth:grant-type:apikey"
+  };
+
+  await request.post(options, function (error, response, body) {
+    const iam_token = JSON.parse(body)["access_token"]
+
+    return res.json({ iam_token })
+  })
+})
+
+routes.post('/floods/weather-conditions/current', async (req, res) => {
+  const { latitude, longitude, iam_token } = req.body
+
+  const { date, time } = getDate()
+
+  const weatherData = { date, lat: latitude, lon: longitude, time }
+
+  const weather = await axios.post(process.env.CLOUD_FUNCTION, weatherData)
+  if (!weather) {
+    return res.status(400).json({ error: "Data mal formatted." })
+  }
+
+  const currentWeather = (weather.data.data.filter((item) => item.date === date && item.uvIndex === 0))[0]
+
+  const { temperature, humidity, windSpeed, precipIntensity } = currentWeather
+
+  const config = {
+    headers: {
+      "Authorization": "Bearer" + iam_token,
+      "Content-Type": "application/json",
+      "ML-Instance-ID": process.env.WATSON_MACHINE_LEARNING_INSTANCE_ID
+    }
+  }
+
+  const floodData = {
+    "input_data": [
+      {
+        "fields": ["latitude", "longitude", "chuva", "temp", "umidade", "vento"],
+        "values": [[latitude, longitude, precipIntensity, temperature, humidity, windSpeed]]
+      }
+    ]
+  }
+
+  await axios.post(process.env.WATSON_MACHINE_LEARNING_URL, floodData, config)
+    .then(response => {
+      const flood_score = response.data.predictions[0].values[0][0]
+
+      return res.json({ ...currentWeather, flood_score })
+    })
+    .catch(err => res.status(500).json("Sorry!! Internal server error."))
+})
+
+routes.post('/floods/weather-conditions/every', async (req, res, next) => {
+  const { latitude, longitude, iam_token } = req.body
+
+  const { date, time } = getDate()
+
+  const weatherData = { date, lat: latitude, lon: longitude, time }
+
+  const weather = await axios.post(process.env.CLOUD_FUNCTION, weatherData)
+  if (!weather) {
+    return res.status(400).json({ error: "Data mal formatted." })
+  }
+
+  const config = {
+    headers: {
+      "Authorization": "Bearer" + iam_token,
+      "Content-Type": "application/json",
+      "ML-Instance-ID": process.env.WATSON_MACHINE_LEARNING_INSTANCE_ID
+    }
+  }
+
+  getEveryScore(weather.data.data, latitude, longitude, config, (newData) => {
+    return res.json(newData)
+  })
 })
 
 module.exports = routes
